257c2155666bed8c6aaec880c0eca01457a06817
Daniel Struebig <47817771+dstrub18@users.noreply.github.com>
2024-05-16 12:20:12 +0200

    analysis window: remove unused field
    * duration does not need to be stored

--- visqol-rs/src/analysis_window.rs
+++ visqol-rs/src/analysis_window.rs
@@ -1,7 +1,5 @@
 /// Temporal analysis window used for creating spectrograms
 pub struct AnalysisWindow {
-    /// Duration of the window in seconds
-    pub window_duration: f64,
     /// Size of the window in samples
     pub size: usize,
     /// Overlap of the window in milliseconds
@@ -12,7 +10,6 @@ impl AnalysisWindow {
     /// Creates a new analysis window based on sample rate, desired overlap and duration
     pub fn new(sample_rate: u32, overlap: f64, window_duration: f64) -> Self {
         Self {
-            window_duration,
             size: (sample_rate as f64 * window_duration).round() as usize,
             overlap,
         }
