644618a164f78a6f0dbaa473aef4173045bb4417
Daniel Struebig <struebig.d@web.de>
2025-08-20 21:19:22 +0200

    feat: improve visqol entry point
    visqol-manager now takes const generics and the variant (wideband or
    fullband)

--- visqol-rs/src/visqol_manager.rs
+++ visqol-rs/src/visqol_manager.rs
@@ -1,80 +1,65 @@
 use std::error::Error;
 
 use crate::{
-    alignment, analysis_window::AnalysisWindow, audio_signal::AudioSignal, audio_utils,
-    comparison_patches_selector::ComparisonPatchesSelector, constants,
+    alignment,
+    analysis_window::AnalysisWindow,
+    audio_signal::AudioSignal,
+    audio_utils,
+    comparison_patches_selector::ComparisonPatchesSelector,
+    constants::{self, PATCH_SIZE_AUDIO, PATCH_SIZE_SPEECH},
     gammatone_filterbank::GammatoneFilterbank,
     gammatone_spectrogram_builder::GammatoneSpectrogramBuilder,
     image_patch_creator::ImagePatchCreator,
     neurogram_similiarity_index_measure::NeurogramSimiliarityIndexMeasure,
-    patch_creator::PatchCreator, similarity_result::SimilarityResult,
+    patch_creator::PatchCreator,
+    similarity_result::SimilarityResult,
     similarity_to_quality_mapper::SimilarityToQualityMapper,
-    spectrogram_builder::SpectrogramBuilder,
     speech_similarity_to_quality_mapper::SpeechSimilarityToQualityMapper,
     svr_similarity_to_quality_mapper::SvrSimilarityToQualityMapper,
-    vad_patch_creator::VadPatchCreator, visqol, visqol_config::VisqolConfig,
+    vad_patch_creator::VadPatchCreator,
+    variant::Variant,
+    visqol,
     visqol_error::VisqolError,
 };
-use log;
 
 /// Configures and executes audio evaluation using ViSQOL.
-pub struct VisqolManager {
+pub struct VisqolManager<const NUM_BANDS: usize> {
     search_window: usize,
     patch_creator: Box<dyn PatchCreator>,
     patch_selector: ComparisonPatchesSelector,
-    spectrogram_builder: Box<dyn SpectrogramBuilder>,
+    spectrogram_builder: GammatoneSpectrogramBuilder<NUM_BANDS>,
     sim_to_quality_mapper: Box<dyn SimilarityToQualityMapper>,
 }
 
-impl VisqolManager {
-    pub fn from_config(config: &VisqolConfig) -> Self {
-        Self::new(
-            &config.similarity_to_quality_model_path,
-            config.use_speech_mode,
-            config.use_unscaled_speech_mos_mapping,
-            config.search_window,
-        )
-    }
+impl<const NUM_BANDS: usize> VisqolManager<NUM_BANDS> {
     /// Creates a new instance of with the desired configurations.
-    pub fn new(
-        model_path: &str,
-        use_speech_mode: bool,
-        use_unscaled_speech_mos_mapping: bool,
-        search_window: usize,
-    ) -> Self {
-        let patch_creator: Box<dyn PatchCreator> = if use_speech_mode {
-            Box::new(VadPatchCreator::new(constants::PATCH_SIZE_SPEECH))
-        } else {
-            Box::new(ImagePatchCreator::new(constants::PATCH_SIZE_AUDIO))
-        };
+    pub fn new(variant: Variant, window_size: usize) -> Self {
+        let patch_creator: Box<dyn PatchCreator>;
+        let sim_to_quality_mapper: Box<dyn SimilarityToQualityMapper>;
+        match variant {
+            Variant::Wideband {
+                use_unscaled_mos_mapping,
+            } => {
+                patch_creator = Box::new(VadPatchCreator::new(PATCH_SIZE_AUDIO));
+                sim_to_quality_mapper = Box::new(SpeechSimilarityToQualityMapper::new(
+                    !use_unscaled_mos_mapping,
+                ));
+            }
+            Variant::Fullband { model_path } => {
+                patch_creator = Box::new(ImagePatchCreator::new(PATCH_SIZE_SPEECH));
+                sim_to_quality_mapper = Box::new(SvrSimilarityToQualityMapper::new(&model_path));
+            }
+        }
 
-        let sim_to_quality_mapper: Box<dyn SimilarityToQualityMapper> = if use_speech_mode {
-            Box::new(SpeechSimilarityToQualityMapper::new(
-                !use_unscaled_speech_mos_mapping,
-            ))
-        } else {
-            Box::new(SvrSimilarityToQualityMapper::new(model_path))
-        };
-
-        let spectrogram_builder: Box<dyn SpectrogramBuilder> = if use_speech_mode {
-            Box::new(GammatoneSpectrogramBuilder::new(
-                GammatoneFilterbank::<{ constants::NUM_BANDS_SPEECH }>::new(
-                    constants::MINIMUM_FREQ,
-                ),
-                use_speech_mode,
-            ))
-        } else {
-            Box::new(GammatoneSpectrogramBuilder::new(
-                GammatoneFilterbank::<{ constants::NUM_BANDS_AUDIO }>::new(constants::MINIMUM_FREQ),
-                false,
-            ))
-        };
+        let spectrogram_builder = GammatoneSpectrogramBuilder::<NUM_BANDS>::new(
+            GammatoneFilterbank::new(constants::MINIMUM_FREQ),
+        );
 
         let patch_selector =
             ComparisonPatchesSelector::new(NeurogramSimiliarityIndexMeasure::default());
 
         Self {
-            search_window,
+            search_window: window_size,
             patch_creator,
             patch_selector,
             spectrogram_builder,
@@ -91,7 +76,7 @@ impl VisqolManager {
         let mut ref_signal = audio_utils::load_as_mono(ref_signal_path)?;
         let mut deg_signal = audio_utils::load_as_mono(deg_signal_path)?;
 
-        self.validate_input_audio(&ref_signal, &deg_signal)?;
+        Self::validate_input_audio(&ref_signal, &deg_signal)?;
 
         self.compute_results(&mut ref_signal, &mut deg_signal)
     }
@@ -113,7 +98,7 @@ impl VisqolManager {
         visqol::calculate_similarity(
             ref_signal,
             &mut deg_signal,
-            self.spectrogram_builder.as_mut(),
+            &mut self.spectrogram_builder, // this does not need to be self
             &window,
             self.patch_creator.as_mut(),
             &self.patch_selector,
@@ -124,7 +109,6 @@ impl VisqolManager {
 
     /// Performs sanity checks on the configuration to prevent incorrect use of the algorithm.
     fn validate_input_audio(
-        &self,
         ref_signal: &AudioSignal,
         deg_signal: &AudioSignal,
     ) -> Result<(), VisqolError> {
