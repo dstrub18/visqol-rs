9c855ce94ab2ee336d7c53053890ebab1602ce01
Daniel Struebig <struebig.d@web.de>
2025-08-20 21:22:24 +0200

    refactor: cli: parse subcommands in main
    instead of demangling each command, let's match on the given subcommand

--- visqol/src/main.rs
+++ visqol/src/main.rs
@@ -3,12 +3,30 @@ use log::LevelFilter;
 use simplelog::{ColorChoice, Config, TermLogger, TerminalMode};
 use std::error::Error;
 
-use visqol_rs::{similarity_result::SimilarityResult, visqol_manager::VisqolManager};
+use visqol_rs::{
+    constants::{NUM_BANDS_AUDIO, NUM_BANDS_SPEECH},
+    similarity_result::SimilarityResult,
+    variant::Variant,
+    visqol_manager::VisqolManager,
+};
 
 pub mod command_line_utils;
 pub mod output_utils;
 pub mod path_pair;
 pub use crate::command_line_utils::{build_file_pair_paths, CommandLineArgs};
+use crate::path_pair::PathPair;
+
+fn run<const NUM_BANDS: usize>(
+    path_pairs: &Vec<PathPair>,
+    visqol: &mut VisqolManager<NUM_BANDS>,
+) -> Result<Vec<SimilarityResult>, Box<dyn Error>> {
+    let mut results = Vec::<SimilarityResult>::with_capacity(path_pairs.len());
+    for file_pair in path_pairs {
+        let result = visqol.run(&file_pair.reference, &file_pair.degraded)?;
+        results.push(result);
+    }
+    Ok(results)
+}
 fn main() -> Result<(), Box<dyn Error>> {
     // Set up logger
     TermLogger::init(
@@ -21,21 +39,34 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Parse arguments
     let args = CommandLineArgs::parse();
 
-    // Figure out file paths
     let files_to_compare = build_file_pair_paths(&args)?;
 
-    let mut results = Vec::<SimilarityResult>::with_capacity(files_to_compare.len());
-    let mut visqol = VisqolManager::new(
-        &args.similarity_to_quality_model,
-        args.use_speech_mode,
-        args.use_unscaled_speech_mos_mapping,
-        args.search_window_radius,
-    );
+    let variant: Variant;
+    let mut visqol_speech: VisqolManager<NUM_BANDS_SPEECH>;
+    let mut visqol_audio: VisqolManager<NUM_BANDS_AUDIO>;
 
-    for file_pair in &files_to_compare {
-        let result = visqol.run(&file_pair.reference, &file_pair.degraded)?;
-        results.push(result);
+    let results: Vec<SimilarityResult>;
+    match &args.subcommand {
+        command_line_utils::Subcommands::Wideband {
+            use_unscaled_speech_mos_mapping,
+        } => {
+            variant = Variant::Wideband {
+                use_unscaled_mos_mapping: *use_unscaled_speech_mos_mapping,
+            };
+            visqol_speech = VisqolManager::new(variant, args.search_window_radius);
+            results = run(&files_to_compare, &mut visqol_speech)?;
+        }
+        command_line_utils::Subcommands::Fullband {
+            similarity_to_quality_model,
+        } => {
+            variant = Variant::Fullband {
+                model_path: similarity_to_quality_model.clone(),
+            };
+            visqol_audio = VisqolManager::new(variant, args.search_window_radius);
+            results = run(&files_to_compare, &mut visqol_audio)?;
+        }
     }
+
     output_utils::write_results(&args, &results, &files_to_compare);
     Ok(())
 }
